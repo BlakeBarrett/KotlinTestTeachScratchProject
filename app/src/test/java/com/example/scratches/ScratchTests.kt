4c796b99451a714a89084555253c44ec52899fec
Blake Barrett <blake@blakebarrett.net>
2021-05-11 16:41:41 -0700

    Commenting out tests that will be part of 'step: 2'

--- app/src/test/java/com/example/scratches/ScratchTests.kt
+++ app/src/test/java/com/example/scratches/ScratchTests.kt
@@ -37,63 +37,64 @@ class ScratchTests {
         assertEquals(chair1, chair2)
     }
 
-    @Test
-    fun testChairClassAndDataClassEquality() {
-        val chair1 = Chair(2)
-        val chair2 = ChairClass(2)
-
-        // can't compare the two directly
-        assertEquals(chair1, chair2)
-
-        // How would you fix this test?
-        //  - only compare the attribute(s) you care about
-        //  - override `fun equals(other: Any?): Boolean`
-        //    • https://kotlinlang.org/docs/equality.html#referential-equality
-    }
-
-    @Test
-    fun testSerializeChair() {
-        val chair = Chair(0, "123abc")
-        val jsonString = Json.encodeToString(chair)
-
-        assertNotNull(jsonString)
-
-        val expected = "{\"numberOfArms\":0,\"serialNumber\":\"123abc\"}"
-
-        assertEquals(expected, jsonString)
-    }
-
-    @Test
-    fun testDeserializeChair() {
-        val jsonString = "{\"numberOfArms\":0,\"serialNumber\":\"123abc\"}"
-        val actual = Json.decodeFromString<Chair>(jsonString)
-
-        assertEquals("123abc", actual.serialNumber)
-    }
-
-    @Test
-    fun testDeserializeChairWithMissingRequiredAttribute() {
-        val jsonString = "{\"serialNumber\":\"123abc\"}"
-        val actual = Json.decodeFromString<Chair>(jsonString)
-
-        assertEquals("123abc", actual.serialNumber)
-    }
-
-    @Test
-    fun testDeserializeChairWithMissingOptionalAttribute() {
-        val jsonString = "{\"numberOfArms\":0}"
-        val actual = Json.decodeFromString<Chair>(jsonString)
-
-        assertEquals(0, actual.numberOfArms)
-
-        assertNull(actual.serialNumber)
-    }
-
-    @Test
-    fun testDeserializeChairWithExtraAttribute() {
-        val jsonString = "{\"numberOfArms\":1,\"serialNumber\":\"123abc\",\"name\":\"Chaise lounge\"}"
-        val actual = Json.decodeFromString<Chair>(jsonString)
-
-        assertEquals("123abc", actual.serialNumber)
-    }
+//    @Test
+//    fun testChairClassAndDataClassEquality() {
+//        val chair1 = Chair(2)
+//        val chair2 = ChairClass(2)
+//
+//        // can't compare the two directly
+//        assertEquals(chair1, chair2)
+//
+//        // How would you fix this test?
+//        //  - only compare the attribute(s) you care about
+//        //  - override `fun equals(other: Any?): Boolean`
+//        //    • https://kotlinlang.org/docs/equality.html#referential-equality
+//        //  - translate values to a common/comparable type
+//    }
+//
+//    @Test
+//    fun testSerializeChair() {
+//        val chair = Chair(0, "123abc")
+//        val jsonString = Json.encodeToString(chair)
+//
+//        assertNotNull(jsonString)
+//
+//        val expected = "{\"numberOfArms\":0,\"serialNumber\":\"123abc\"}"
+//
+//        assertEquals(expected, jsonString)
+//    }
+//
+//    @Test
+//    fun testDeserializeChair() {
+//        val jsonString = "{\"numberOfArms\":0,\"serialNumber\":\"123abc\"}"
+//        val actual = Json.decodeFromString<Chair>(jsonString)
+//
+//        assertEquals("123abc", actual.serialNumber)
+//    }
+//
+//    @Test
+//    fun testDeserializeChairWithMissingRequiredAttribute() {
+//        val jsonString = "{\"serialNumber\":\"123abc\"}"
+//        val actual = Json.decodeFromString<Chair>(jsonString)
+//
+//        assertEquals("123abc", actual.serialNumber)
+//    }
+//
+//    @Test
+//    fun testDeserializeChairWithMissingOptionalAttribute() {
+//        val jsonString = "{\"numberOfArms\":0}"
+//        val actual = Json.decodeFromString<Chair>(jsonString)
+//
+//        assertEquals(0, actual.numberOfArms)
+//
+//        assertNull(actual.serialNumber)
+//    }
+//
+//    @Test
+//    fun testDeserializeChairWithExtraAttribute() {
+//        val jsonString = "{\"numberOfArms\":1,\"serialNumber\":\"123abc\",\"name\":\"Chaise lounge\"}"
+//        val actual = Json.decodeFromString<Chair>(jsonString)
+//
+//        assertEquals("123abc", actual.serialNumber)
+//    }
 }
